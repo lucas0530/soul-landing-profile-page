ef8ec35ae7d3208e2b72d8882e18a43a5f50356c
KZ-Lucas <qq3884qq@naver.com>
2022-08-12 06:52:23 +0900

    feature: Archiving 섹션 개발 처리

--- src/components/atoms/Text.style.tsx
+++ src/components/atoms/Text.style.tsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled'
+import { TextProps } from './Text'
 
-export const Span = styled.span`
+export const Span = styled.span<TextProps>`
   white-space: break-spaces;
+  color: ${(props) => props.theme.colors.black};
+  font-weight: ${(props) => (props.bold ? 'bold' : 'medium')};
 `
