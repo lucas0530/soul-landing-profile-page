5e06ea7b3f22d4c0f2034e07f4e620611d7fd773
KZ-Lucas <qq3884qq@naver.com>
2022-08-15 03:08:57 +0900

    이미지 추가 및 헤더 변경

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>최근원's 포트폴리오</title>
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
